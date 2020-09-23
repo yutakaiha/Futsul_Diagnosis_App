7bea5ad56eae195deacd70fb85063dfeccdfe562
yuta <fantasista098@gmail.com>
2020-09-23 10:22:48 +0900

    question_numberの修正と質問処理をループ処理に再修正

--- question/question.rb
+++ question/question.rb
@@ -1,18 +1,18 @@
-require "./module/act_question_module"
-
+# require "./module/act_question_module"
+require "./module/loop_question"
 class Question
-  include ActQuestion
+  include LoopQuestion
   attr_reader :title, :questions, :text
 
-  def start_question(calucu_results, special_addition_list)
-    obj_result = self.questions_start
-    self.add_list(obj_result, calucu_results, special_addition_list)
-  end
+  # def start_question(calucu_results, special_addition_list)
+  #   obj_result = self.questions_start
+  #   self.add_list(obj_result, calucu_results, special_addition_list)
+  # end
 
-  def add_list(obj_result, calucu_results, special_addition_list)
-    calucu_results << obj_result
-    special_addition_list << obj_result.select{ |e| e.length == 1 }
-    calucu_results.flatten!
-    special_addition_list.flatten!
-  end
-end
+  # def add_list(obj_result, calucu_results, special_addition_list)
+  #   calucu_results << obj_result
+  #   special_addition_list << obj_result.select{ |e| e.length == 1 }
+  #   calucu_results.flatten!
+  #   special_addition_list.flatten!
+  # end
+end 
